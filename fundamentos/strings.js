c2c689f56149bbf7bc89a56b2df050068d3a5f17
Chrystyan allyson <chrystyan.allyson@gmail.com>
2022-10-18 13:16:35 -0300

    aulas de js

---
+++ fundamentos/strings.js
@@ -0,0 +1,16 @@
+const escola = "Cod3r";
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(5));
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf('d'));
+//substring
+console.log(escola.substring(1));
+console.log(escola.substring(0,3));
+
+console.log('Esccola '.concat(escola).concat("!"));
+console.log('Esccola '+ escola +"!");
+
+console.log(escola.replace(3,'e'));
+
+console.log('Ana,Maria,Pedro'.split(','));
